c7b1362f79a7f1f3d2420c1b5ceafaeaf20d6770
stxss <93729036+stxss@users.noreply.github.com>
2023-05-08 18:08:23 +0100

    Create references from Flight to Airports

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ApplicationRecord
-  # belongs_to :departure, class_name: "Airport"
-  # belongs_to :arrival, class_name: "Airport"
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :arrival_airport, class_name: "Airport"
 end
